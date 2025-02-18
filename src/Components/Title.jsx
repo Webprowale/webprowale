78e3f6856b8b4a4c72da2ffadf7e405760653b96
Joshua Akinwale(Webprowale) <129720346+Webprowale@users.noreply.github.com>
2025-02-18 13:50:42 +0100

    Update Title.jsx

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -16,7 +16,7 @@ function Title() {
     <div className='rounded shadow px-3  pt-4 pb-2 d-flex flex-column con1' ref={con}>
      <h3 className='fs-6 small fw-semibold primaryColor tit'>Title</h3>
      <h2 className='fw-bold fs-6 ms-2 text-black sof'>Software</h2>
-     <h2 className='fw-bold fs-4 ms-3 lead dev'>Developer</h2>
+     <h2 className='fw-bold fs-4 ms-3 lead dev'>Engineer</h2>
     </div>
   )
 }
