c1aa4ae612e9f14b19fa004a4ebcc530d9493a7d
Joshua Akinwale(Webprowale) <129720346+Webprowale@users.noreply.github.com>
2025-06-24 00:44:03 +0100

    Update Experience.jsx

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -14,15 +14,15 @@ function Experience() {
   return (
     <div className="rounded mt-3 shadow px-3 py-4 d-flex flex-column text-center con1" ref={con} >
     <div className="d-flex flex-column ">
-    <h3>04</h3>
+    <h3>05</h3>
     <p className='lead fs-4'>YEARS OF Experience</p>
     </div>
     <div className="d-flex flex-column">
-    <h3>10+</h3>
+    <h3>30+</h3>
     <p className='lead fs-4'>TOTAL PROJECTS</p>
     </div>
     <div className="d-flex flex-column mb-4">
-    <h3>7+</h3>
+    <h3>15+</h3>
     <p className='lead fs-4'>CLIENTS WORLDWIDE</p>
     </div>
     </div>
