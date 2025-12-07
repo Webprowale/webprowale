a15a09b5a5a572920f4c7bfd6319d2648317cbd1
Webprowale <webprowale@gmail.com>
2025-12-07 20:16:17 +0100

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,64 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Webprowale Portfolio - Joshua Akinwale',
+        short_name: 'Webprowale',
+        description: 'Full Stack Software Engineer Portfolio',
+        theme_color: '#8121d0',
+        background_color: '#dccfed',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          animations: ['gsap', '@gsap/react', 'framer-motion'],
+          utils: ['axios', 'formik', 'yup']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 });
